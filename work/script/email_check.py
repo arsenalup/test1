ca87df16c216fb535612d2db6c89e85b369fd25f
chen keyan <676250063@qq.com>
2017-10-09 21:56:18 +0800

    10.9

---
+++ work/script/email_check.py
@@ -0,0 +1,11 @@
+import re
+
+
+class spider():
+    def run(self, url, html):
+        pattern = re.compile(r'([\w-]+@[\w-]+\.[\w-]+)+')
+        email_list = re.findall(pattern, html)
+        if (email_list):
+            print(email_list)
+            return True
+        return False
